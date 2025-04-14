4f205fb332f05019915a58795419e3fdc6a3cf3d
adititiwari0810 <aditiwari0810@gmail.com>
2025-04-14 14:57:26 +0530

    updated code

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -11,12 +11,10 @@ export const Header = () => {
     })
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/logo.svg'} alt='logo' width={160} height={100}  style={{ width: "auto", height: "auto" }}   />
+        <Image src={'/logo.svg'} alt='logo' width={90} height={90}     />
         <ul className='hidden md:flex gap-6'>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How its works?</li>
+        
         </ul>
         <UserButton/>
     </div>
--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -110,7 +110,7 @@ const RecordAnswerSection = ({
   if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
   return (
     <div className="flex justify-cente items-center flex-col">
-      <div className="flex flex-col my-20 justify-center items-center bg-black rounded-lg p-5">
+      <div className="flex flex-col my-20 justify-center items-center bg-black rounded-lg p-1">
         <Image
           src={"/webcam.png"}
           width={200}
@@ -119,10 +119,10 @@ const RecordAnswerSection = ({
           alt="webcam"
           priority
         />
-        {/* <Webcam
+        { <Webcam
           style={{ height: 300, width: "100%", zIndex: 10 }}
           mirrored={true}
-        /> */}
+        /> } 
       </div>
       <Button
         disabled={loading}
